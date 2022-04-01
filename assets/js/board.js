08bcf0317d87aacae8e662c9075e09e0655537fd
rashidya <rashmiliyanage2000@gmail.com>
2022-04-02 02:36:58 +0530

    Game board css and js updated

--- assets/js/board.js
+++ assets/js/board.js
@@ -1,23 +1,56 @@
-var selectedCard;
+var selectedCard=null;
 var selectedImage;
 var selectedIcon;
 setImages();
 $(".cards").click(function (){
-    if(this==selectedCard){
+    $(selectedCard).off('mouseout');
+    if(selectedCard==null || this!=selectedCard){
+        $(this).css('transform','rotateY(180deg)');
+        $(this).css('transition','all 1s');
+        $(this.firstChild).css('display','block');
+        $(this).children('img:nth-child(2)').css('display','none');
+
+        if(this!=selectedCard && selectedCard!=null){
+
+            if( $(this).children('img:nth-child(1)').attr('src')== $(selectedIcon).attr('src')){
+                console.log(2)
+                $(this).css('visibility','hidden');
+                $(selectedCard).css('visibility','hidden');
+            }else {
+                console.log(3)
+                console.log($(selectedIcon).attr('src'))
+                console.log($(this).children('img:nth-child(1)').attr('src'));
+
+                   /* $(this).css('transform','rotateY(0deg)');
+                    $(this).css('transition','all 1s');
+                    $(this).css('transition-delay','all 1s');
+                    $(this.firstChild).css('display','none');
+                    $(this).children('img:nth-child(2)').css('display', 'block');
+                    $(selectedCard).css('transform','rotateY(0deg)');
+                    $(selectedCard).css('transition','all 2s');
+                    $(selectedCard).css('transition-delay','all 2s');
+                    $(selectedCard).children('img:nth-child(1)').css('display','none');
+                    $(selectedCard).children('img:nth-child(2)').css('display','block');*/
+
+
+            }
+
+
+        }
+
+        selectedCard =this;
+        selectedImage =$(this).children('img:nth-child(2)');
+        selectedIcon =$(this).children('img:nth-child(1)');
+    }else{
+        console.log(1)
         $(this).css('transform','rotateY(0deg)');
         $(this).css('transition','all 1s');
         $(this.firstChild).css('display','none');
-
+        $(this).children('img:nth-child(2)').css('display','block');
+        selectedCard=null;
     }
-    
-    $(this).css('transform','rotateY(180deg)');
-    $(this).css('transition','all 1s');
-    $(this.firstChild).css('display','block');
-    $(this).children('img:nth-child(2)').css('display','none');
 
-    $(selectedCard).css('transform','rotateY(0deg)');
-    $(selectedImage).css('display','block');
-    $(selectedIcon).css('display','none');
+
 
 /*    if(this.firstChild.attr('src')==selectedImage.attr('src')){
         $(selectedCard).css('display','none)');
@@ -33,9 +66,6 @@ $(".cards").click(function (){
     */
 
 
-    selectedCard =this;
-    selectedImage =$(this).children('img:nth-child(2)');
-    selectedIcon =$(this).children('img:nth-child(1)');
 
 });
 
@@ -45,7 +75,6 @@ function setImages(){
     var divSet=$(".icon");
 
     for (let i = 0; i <iconList.length; i++) {
-        console.log(i)
         for (let j = 0; j < 2; j++) {
           while (loop){
                 var id=Math.floor(Math.random()*16);
@@ -54,8 +83,6 @@ function setImages(){
                     $(divSet[id]).attr('src',iconList[i]);
                     numbers.push(id);
                     loop=false;
-
-                    console.log(iconList[i])
                 }
           }
 
